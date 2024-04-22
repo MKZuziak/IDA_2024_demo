5215c911eae051460001b88f646fedbff52c796a
Scolpe <maciejzuziak101@gmail.com>
2024-04-22 17:03:19 +0200

    Updates forcha version

--- ida_2024_demo/ida_2024_demo/model.py
+++ ida_2024_demo/ida_2024_demo/model.py
@@ -1,11 +1,15 @@
 import json
-from numpy.random import uniform
+import time
+import pickle
 import os
+from numpy.random import uniform
 from fedata.hub.generate_dataset import generate_dataset
+import datasets
 from forcha.components.orchestrator.evaluator_orchestrator import Evaluator_Orchestrator
-from forcha.components.settings.init_settings import init_settings
+from forcha.components.settings.evaluator_settings import EvaluatorSettings
 from models.mnist_fmnist import MNIST_CNN
 from models.cifar_resnet import ResNet18
+
 
 def fetch_json(path):
     with open(path) as j_file:
@@ -31,89 +35,52 @@ def fetch_data_configuration(parsed_file):
     "alpha": 0.5}
 
 
-def fetch_simulation_configuration(parsed_file):
-    return {
-        "orchestrator": {
-        "iterations": parsed_file['iterations'],
-        "number_of_nodes": parsed_file['clients'],
-        "sample_size": parsed_file['sample_size'],
-        'enable_archiver': True,
-        "archiver":{
-            "root_path": os.getcwd(),
-            "orchestrator": True,
-            "clients_on_central": True,
-            "central_on_local": True,
-            "log_results": True,
-            "save_results": True,
-            "save_orchestrator_model": True,
-            "save_nodes_model": True,
-            "form_archive": True
-            },
-        "optimizer": {
-            "name": "Simple",
-            "learning_rate": parsed_file['global_learning_rate']},
-        "evaluator" : {
-        "LOO_OR": False,
-        "Shapley_OR": False,
-        "IN_SAMPLE_LOO": True,
-        "IN_SAMPLE_SHAP": False,
-        "LSAA": True,
-        "EXTENDED_LSAA": False,
-        "ADAPTIVE_LSAA": False,
-        "line_search_length": parsed_file['alpha-amplification'],
-        "preserve_evaluation": {
-            "preserve_partial_results": True,
-            "preserve_final_results": True},
-        "full_debug": True,
-        "number_of_workers": 50}},
-    "nodes":{
-    "local_epochs": parsed_file['local_epochs'],
-    "model_settings": {
-        "optimizer": "Adam",
-        "betas": (0.9, 0.8),
-        "weight_decay": 1e-4,
-        "amsgrad": True,
-        "batch_size": parsed_file['batch_size'],
-        "learning_rate": parsed_file['learning_rate'],
-        "gradient_clip": 2,
-        "FORCE_CPU": False}}}
-
-
 def main(path = None,
          from_json: bool = True,
          config: dict = None,
          savepath = None):
+    
     if from_json:
         config = fetch_json(path)
     data_config = fetch_data_configuration(config)
-    simulation_config = fetch_simulation_configuration(config)
     
     os.chdir(savepath)
     # generating datset
     loaded_dataset = generate_dataset(config=data_config)
+    orchestrator_data = loaded_dataset[0]
+    nodes_data = loaded_dataset[1]
     
-    settings = init_settings(
-        orchestrator_type='evaluator',
-        initialization_method='dict',
-        dict_settings=simulation_config,
-        allow_default=True
-    )
-    
+    settings = EvaluatorSettings(
+        simulation_seed=42,
+        global_epochs=config['iterations'],
+        local_epochs=config['local_epochs'],
+        number_of_nodes=config['clients'],
+        sample_size=config['sample_size'],
+        optimizer='SGD',
+        batch_size=config['batch_size'],
+        learning_rate=config['learning_rate'],
+        in_sample_loo=True,
+        in_sample_shap=False,
+        in_sample_alpha=True,
+        line_search_length=config['alpha-amplification'])
+
     if config['dataset'] == 'mnist' or config['dataset'] == 'fmnist':
         model = MNIST_CNN()
     else:
         model = ResNet18()
     
-    # Setting-up the orchestrator
-    orchestrator = Evaluator_Orchestrator(settings)
+    orchestrator = Evaluator_Orchestrator(
+        settings=settings, 
+        full_debug=True,
+        parallelization=True,
+        number_of_workers=2
+        )
     orchestrator.prepare_orchestrator(
-        model=model,
-        validation_data=loaded_dataset[0]
-    )
-    orchestrator.prepare_training(nodes_data=loaded_dataset[1])
+        model=model, 
+        validation_data=orchestrator_data)
+    orchestrator.prepare_training(nodes_data=nodes_data)
+    
     signal = orchestrator.train_protocol()
-    print(signal)
-
 
 if __name__ == '__main__':
     main()
